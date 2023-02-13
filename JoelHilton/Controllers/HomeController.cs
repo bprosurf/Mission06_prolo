1bdaf260990720b7d6561343a15c525330e99176
Brad Prolo <bprosurf@yahoo.com>
2023-02-12 19:01:09 -0700

    Add project files.

---
+++ JoelHilton/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using JoelHilton.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoelHilton.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieAppContext _blahContext { get; set; }
+        public HomeController(ILogger<HomeController> logger, MovieAppContext someName)
+        {
+            _logger = logger;
+            _blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Movies(ApplicationResponse ar)
+        {
+            _blahContext.Add(ar);
+            _blahContext.SaveChanges();
+
+            return View("Confirmation", ar);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JoelHilton/Models/MovieAppContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoelHilton.Models
+{
+    public class MovieAppContext : DbContext
+    {
+        //Constructor
+        public MovieAppContext (DbContextOptions<MovieAppContext> options): base(options)
+        {
+            //Leave Blank for now
+        }
+
+        public DbSet<ApplicationResponse> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+                
+                new ApplicationResponse
+                {
+                    MovieId = 1,
+                    Category = "Adventure/Drama/Sci-Fi",
+                    Title = "Intersteller",
+                    Year = 2014,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+
+
+                },
+                new ApplicationResponse
+                {
+                    MovieId = 2,
+                    Category = "Mystery/Thiller",
+                    Title = "Shutter Island",
+                    Year = 2010,
+                    Director = "Martin Scorsese",
+                    Rating = "R",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+
+
+                },
+                new ApplicationResponse
+                {
+                MovieId = 3,
+                    Category = "Drama/Mystery/Sci-Fi",
+                    Title = "The Prestige",
+                    Year = 2006,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+
+
+                }
+            );
+        }
+    }
+}
